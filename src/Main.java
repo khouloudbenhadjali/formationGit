7fd15f37ff99a9c2121b94536716bbbd0f38a374
Khouloudbha <khouloudbha8991@gmail.com>
2023-08-30 22:58:37 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 
         System.out.println("Hello world!");
-        System.out.println("test branch 01");
         System.out.println(" be happy");
         System.out.println(" test branch 02");
         System.out.println(" Test 2 branch 02");
