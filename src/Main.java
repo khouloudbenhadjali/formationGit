b96f2a96a2cd44a63529d472b49701dbf04dff89
Khouloudbha <khouloudbha8991@gmail.com>
2023-08-30 22:55:28 +0200

    add some test of branches

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("test branch 01");
         System.out.println(" be happy");
+        System.out.println(" test branch 02");
+        System.out.println(" Test 2 branch 02");
     }
 }
